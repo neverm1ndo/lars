9a711b0e5c2a33b7ebf2012170d9ee266373e5ac
neverm1ndo <it@nmnd.ru>
2024-08-05 00:49:11 +0300

    modified:   apps/lars/src/app/app.module.ts 	modified:   apps/lars/src/app/topbar/topbar.component.html 	modified:   apps/lars/src/app/topbar/topbar.component.scss 	new file:   apps/lars/src/scss/_variables.scss 	modified:   apps/lars/src/styles.scss

--- apps/lars/src/app/app.module.ts
+++ apps/lars/src/app/app.module.ts
@@ -6,16 +6,36 @@ import {
 } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
 import { CoreModule } from '@lars/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './topbar/topbar.component';
 
+const MAT_MODULES = [
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule
+];
+
 @NgModule({
   declarations: [AppComponent, TopbarComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
-  providers: [provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync()],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    ...MAT_MODULES
+  ],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()), 
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
