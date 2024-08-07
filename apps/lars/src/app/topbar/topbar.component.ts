89f8ce54d87f2fc1cf0f71d9fe36648b7e53334b
neverm1ndo <it@nmnd.ru>
2024-08-07 15:05:10 +0300

    chore(topbar) add window controls

--- apps/lars/src/app/topbar/topbar.component.ts
+++ apps/lars/src/app/topbar/topbar.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ElectronService } from '@lars/core';
 
 @Component({
-  selector: 'lars-topbar',
-  templateUrl: './topbar.component.html',
-  styleUrl: './topbar.component.scss',
+    selector: 'lars-topbar',
+    templateUrl: './topbar.component.html',
+    styleUrl: './topbar.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TopbarComponent {}
+export class TopbarComponent {
+    private readonly electron = inject(ElectronService);
+    
+    close(): void {
+        void this.electron.ipcRenderer?.send('close');
+    }
+    minimize(): void {
+        void this.electron.ipcRenderer?.send('minimize');
+    }
+}
