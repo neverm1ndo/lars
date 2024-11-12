12d0b711b5da13105f56d9cf5c6dcbfc866e7794
neverm1ndo <it@nmnd.ru>
2024-11-12 20:39:42 +0300

    chore: Add empty state for dashboards

--- libs/dashboard/feature-dashboard/src/lib/containers/feature-dashboard.component.ts
+++ libs/dashboard/feature-dashboard/src/lib/containers/feature-dashboard.component.ts
@@ -1,8 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GridsterConfig, GridsterItem } from 'angular-gridster2';
 
 @Component({
   selector: 'lars-feature-dashboard',
   templateUrl: './feature-dashboard.component.html',
   styleUrl: './feature-dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  options!: GridsterConfig;
+  dashboard!: Array<GridsterItem>;
+
+  itemChange(item: any, itemComponent: any) {
+    console.info('itemChanged', item, itemComponent);
+  }
+
+  itemResize(item: any, itemComponent: any) {
+    console.info('itemResized', item, itemComponent);
+  }
+
+  changedOptions() {
+    if (this.options.api) {
+      // this.options.api.optionsChanged();
+    }
+  }
+
+  removeItem(item: any) {
+    this.dashboard.splice(this.dashboard.indexOf(item), 1);
+  }
+
+  addItem() {
+    // this.dashboard.push({});
+  }
+
+  ngOnInit(): void {
+    this.options = {
+      itemChangeCallback: this.itemChange,
+      itemResizeCallback: this.itemResize,
+      draggable: {
+        enabled: true
+      },
+      maxCols: 4
+    };
+
+    this.dashboard = [
+      // {cols: 2, rows: 1, y: 0, x: 0 },
+      // {cols: 2, rows: 2, y: 0, x: 2 }
+    ];
+  }
+}
--- libs/dashboard/feature-dashboard/src/lib/dashboard.module.ts
+++ libs/dashboard/feature-dashboard/src/lib/dashboard.module.ts
@@ -1,9 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { GridsterModule } from 'angular-gridster2';
+
 import { DashboardComponent } from './containers/feature-dashboard.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+const MATERIAL_MODULES = [
+  MatTabsModule,
+  MatButtonModule,
+  MatIconModule
+];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    GridsterModule,
+    ...MATERIAL_MODULES
+  ],
   declarations: [DashboardComponent],
   exports: [DashboardComponent],
 })
